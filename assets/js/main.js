8ebc77f8b2f000b3ff6f9c453a5b81a67cdec028
Abhishek Singh <singh.msoe@gmail.com>
2023-06-12 15:59:07 +0530

    lot of changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(function() {
     //===== Prealoder
     
     $(window).on('load', function(event) {
-        $('.preloader').delay(500).fadeOut(500);
+        $('.preloader').delay(100).fadeOut(100);
     });
     
     
@@ -125,9 +125,9 @@ $(function() {
     // Show or hide the sticky footer button
     $(window).on('scroll', function(event) {
         if($(this).scrollTop() > 600){
-            $('.back-to-top').fadeIn(200)
+            $('.back-to-top').fadeIn(20)
         } else{
-            $('.back-to-top').fadeOut(200)
+            $('.back-to-top').fadeOut(20)
         }
     });
     
